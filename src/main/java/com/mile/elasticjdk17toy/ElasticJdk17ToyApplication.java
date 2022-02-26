23cb6274950b1f28bbecff5e8ae05a96f8cebeeb
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-02-26 22:36:45 +0900

    Initial commit

---
+++ src/main/java/com/mile/elasticjdk17toy/ElasticJdk17ToyApplication.java
@@ -0,0 +1,13 @@
+package com.mile.elasticjdk17toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticJdk17ToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJdk17ToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mile/elasticjdk17toy/ElasticJdk17ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mile.elasticjdk17toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticJdk17ToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
